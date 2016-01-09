948a6b1363a9804fcb983d313b36f7a19e6e5507
Federico Ferri <federico.ferri.it@gmail.com>
2016-01-09 19:33:51 +0100

    Initial Commit

---
+++ LISP/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LISP
+//
+//  Created by Federico Ferri on 09/01/2016.
+//  Copyright © 2016 Federico Ferri. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
