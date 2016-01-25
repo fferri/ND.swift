98c4619b49ad72ad51ec6818d9cda14258118859
Federico Ferri <federico.ferri.it@gmail.com>
2016-01-25 23:06:09 +0100

    make op optional in NAryOperator.parse so it can parse list expressions

--- src/expr/nary_operator.swift
+++ src/expr/nary_operator.swift
@@ -5,11 +5,13 @@ public class NAryOperator : Expr {
         fatalError()
     }
     
-    class func parse(ts: TokenStream, op: String, minArity: Int? = nil, maxArity: Int? = nil) -> [Expr]? {
+    class func parse(ts: TokenStream, op: String? = nil, minArity: Int? = nil, maxArity: Int? = nil) -> [Expr]? {
         let oldpos = ts.pos
         parse: do {
             guard let t1 = ts.read() where t1.value == "(" else {break parse}
-            guard let t2 = ts.read() where t2.value == op else {break parse}
+            if op != nil {
+                guard let t2 = ts.read() where t2.value == op! else {break parse}
+            }
             var e = [Expr]()
             while let en = Expr.parse(ts) {
                 e.append(en)
